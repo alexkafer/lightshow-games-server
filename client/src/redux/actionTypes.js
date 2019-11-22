acca5ce6435ff349436f5c80cb8dcd8ae05300c4
Alex Kafer <me@alexkafer.com>
2019-11-22 09:17:38 -0600

    Move client in its own repository

--- client/src/redux/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const DEVICE_MOTION = "DEVICE_MOTION";
-export const DEVICE_ORIENTATION = "DEVICE_ORIENTATION";
-
-export const START_GAME = "START_GAME";
-export const END_GAME = "END_GAME";
-export const LOAD_GAME = "LOAD_GAME";
-export const QUEUE_UPDATE = "QUEUE_UPDATE";
--- client/src/redux/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import gyro from "./gyro";
-import game from "./game";
-
-export default combineReducers({gyro, game});
